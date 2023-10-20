e221a1669c673da59837345ffa9261a32fc8635d
JSEGUR <jeremy.segur@etu.inp-n7.fr>
2023-10-20 08:58:23 +0200

    feature/new_database: Matteo Login Issue

--- app.js
+++ app.js
@@ -7,7 +7,11 @@ import sqlite3 from 'sqlite3';
 import session from 'express-session';
 import bodyParser from 'body-parser';
 import SQLiteStore from 'connect-sqlite3';
+<<<<<<< HEAD
 import fs from 'fs';
+=======
+import fs from 'node:fs';
+>>>>>>> 72cd5d8 (wip)
 
 // Configuration de l'application Express pour traiter les données de formulaire
 const app = express();
@@ -29,11 +33,99 @@ app.get('/', function (request, response) {
   response.sendFile(__public + "/login_page/login.html");
 });
 
-// Ouverture du fichier html du formulaire de création d'un nouvel utilisateur
-app.get('/create_user', function (request, response) {
-  response.sendFile(__public + "/login_page/create_user.html");
+// Créer une route pour récupérer les Users de la bd
+app.get('/api/getUsers', (req, res) => {
+  const query = 'SELECT * FROM users';
+  db.all(query, (err, rows) => {
+      if (err) {
+          res.status(500).json({ error: err.message });
+          return;
+      }
+
+      // Créez le code HTML à renvoyer
+      let html = '<!DOCTYPE html><html><head><title>Liste des utilisateurs</title></head><body>';
+      html += '<div id="dataContainer"><table><tr><th>Username</th><th>Password</th></tr>';
+
+      rows.forEach(row => {
+          html += `<tr><td>${row.username}</td><td>${row.password}</td></tr>`;
+      });
+
+      html += '</table></div></body></html>';
+
+      // Afficher les données dans la console
+      console.log(rows)
+      // Renvoyez le code HTML
+      res.send(html);
+  });
 });
 
+
+// Créer une route pour récupérer les Files de la bd
+app.get('/api/getFiles', (req, res) => {
+  const query = 'SELECT * FROM files';
+  db.all(query, (err, rows) => {
+    if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+    }
+
+    // Créez le code HTML à renvoyer
+    let html = '<!DOCTYPE html><html><head><title>Liste des fichiers</title></head><body>';
+    html += '<div id="dataContainer"><table><tr><th>ID</th><th>Directory</th><th>Name</th><th>File</th></tr>';
+
+    rows.forEach(row => {
+        html += `<tr><td>${row.id}</td><td>${row.directory}</td><td>${row.name}</td><td>${row.file}</td></tr>`;
+    });
+
+    html += '</table></div></body></html>';
+
+    // Afficher les données dans la console
+    console.log(rows)
+    // Renvoyez le code HTML
+    res.send(html);
+  });
+});
+
+// Créer une route pour récupérer les Directories de la bd
+app.get('/api/getDirectories', (req, res) => {
+  const query = 'SELECT * FROM directories';
+  db.all(query, (err, rows) => {
+    if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+    }
+
+    // Créez le code HTML à renvoyer
+    let html = '<!DOCTYPE html><html><head><title>Liste des répertoires</title></head><body>';
+    html += '<div id="dataContainer"><table><tr><th>ID</th><th>Directory</th><th>Name</th></tr>';
+
+    rows.forEach(row => {
+        html += `<tr><td>${row.id}</td><td>${row.directory}</td><td>${row.name}</td></tr>`;
+    });
+
+    html += '</table></div></body></html>';
+
+    // Afficher les données dans la console
+    console.log(rows)
+    // Renvoyez le code HTML
+    res.send(html);
+  });
+});
+
+// Ouverture du fichier html du formulaire de création d'un nouvel utilisateur
+app.get('/create_user', function (request, response) {
+  db.get('SELECT * FROM users', (err, row) => {
+    if (err) {
+      console.error('Erreur Liste des users :', err.message);
+      res.redirect("/create_user");
+    }
+    if (row) {
+      //console.log(row.rows.item(0));
+    }
+  }
+)
+  response.sendFile(__public + "/login_page/create_user.html");
+});
 
 // Création d'une connexion à la base de données SQLite
 
@@ -68,6 +160,7 @@ app.post('/creer_utilisateur', (req, res) => {
 
     (err) => {
       if (err) {
+<<<<<<< HEAD
         console.error('Erreur lors de l\'insertion de l\'utilisateur :', err.message);
         res.cookie("user_creation", "Erreur lors de l\'insertion de l\'utilisateur :");
         res.redirect("/");
@@ -86,6 +179,37 @@ app.post('/creer_utilisateur', (req, res) => {
             // Dossier utilisateur créé avec succès
             // Répondez avec un message de succès ou effectuez d'autres actions nécessaires
             console.log('Dossier utilisateur créé avec succès');
+=======
+        console.error('Erreur lors de la vérification du login :', err.message);
+        res.redirect("/create_user");
+      }
+      if (row) {
+        console.error('Login déja existant');
+        res.cookie("user_creation", "Login déja existant");
+        res.redirect("/create_user");
+      }else{
+        fs.mkdir(__public + '/storage/' + username, (error) => {
+          if (error) {
+            console.log(error);
+          } else {
+            console.log(username + " directory created successfully !!");
+          }
+        });
+        db.run(
+          'INSERT INTO users (username, password) VALUES (?, ?)',
+          [username, password],
+  
+          (err) => {
+              if (err) {
+                  console.error('Erreur lors de l\'insertion de l\'utilisateur :', err.message);
+                  res.cookie("user_creation", "Erreur lors de l\'insertion de l\'utilisateur :");
+                  res.redirect("/");
+              } else {
+                  console.log('Utilisateur créé avec succès.');
+                  res.cookie("user_creation", "Utilisateur créé avec succès.");
+                  res.redirect("/");
+              }
+>>>>>>> 72cd5d8 (wip)
           }
         });
         //creer un fichier dans la bd pour l'user
