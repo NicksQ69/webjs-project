91a4372f83082fa4b0b9200a041eef7c36a8e024
JSEGUR <jeremy.segur@etu.inp-n7.fr>
2023-10-20 10:42:47 +0200

    wip

--- app.js
+++ app.js
@@ -36,19 +36,48 @@ app.get('/api/getUsers', (req, res) => {
           res.status(500).json({ error: err.message });
           return;
       }
+
+      // Créez le code HTML à renvoyer
+      let html = '<!DOCTYPE html><html><head><title>Liste des utilisateurs</title></head><body>';
+      html += '<div id="dataContainer"><table><tr><th>Username</th><th>Password</th></tr>';
+
+      rows.forEach(row => {
+          html += `<tr><td>${row.username}</td><td>${row.password}</td></tr>`;
+      });
+
+      html += '</table></div></body></html>';
+
+      // Afficher les données dans la console
       console.log(rows)
+      // Renvoyez le code HTML
+      res.send(html);
   });
 });
+
 
 // Créer une route pour récupérer les Files de la bd
 app.get('/api/getFiles', (req, res) => {
   const query = 'SELECT * FROM files';
   db.all(query, (err, rows) => {
-      if (err) {
-          res.status(500).json({ error: err.message });
-          return;
-      }
-      console.log(rows)
+    if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+    }
+
+    // Créez le code HTML à renvoyer
+    let html = '<!DOCTYPE html><html><head><title>Liste des fichiers</title></head><body>';
+    html += '<div id="dataContainer"><table><tr><th>ID</th><th>Directory</th><th>Name</th><th>File</th></tr>';
+
+    rows.forEach(row => {
+        html += `<tr><td>${row.id}</td><td>${row.directory}</td><td>${row.name}</td><td>${row.file}</td></tr>`;
+    });
+
+    html += '</table></div></body></html>';
+
+    // Afficher les données dans la console
+    console.log(rows)
+    // Renvoyez le code HTML
+    res.send(html);
   });
 });
 
@@ -56,11 +85,25 @@ app.get('/api/getFiles', (req, res) => {
 app.get('/api/getDirectories', (req, res) => {
   const query = 'SELECT * FROM directories';
   db.all(query, (err, rows) => {
-      if (err) {
-          res.status(500).json({ error: err.message });
-          return;
-      }
-      console.log(rows)
+    if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+    }
+
+    // Créez le code HTML à renvoyer
+    let html = '<!DOCTYPE html><html><head><title>Liste des répertoires</title></head><body>';
+    html += '<div id="dataContainer"><table><tr><th>ID</th><th>Directory</th><th>Name</th></tr>';
+
+    rows.forEach(row => {
+        html += `<tr><td>${row.id}</td><td>${row.directory}</td><td>${row.name}</td></tr>`;
+    });
+
+    html += '</table></div></body></html>';
+
+    // Afficher les données dans la console
+    console.log(rows)
+    // Renvoyez le code HTML
+    res.send(html);
   });
 });
 
